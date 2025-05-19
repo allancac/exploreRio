890aab4227f1535671968b3fbf0d2a8fb592906a
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-19 12:54:26 -0300

    Initial commit

---
+++ src/main/java/com/example/explorerio/ExploreRioApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreRioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreRioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/explorerio/ExploreRioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorerio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreRioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
