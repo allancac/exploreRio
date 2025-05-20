5aa2c42e36be373947475048827bb68b223e274c
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-20 18:44:26 -0300

    test: Add manual tests for the Models in the class CLRunner
    - Trail and TourPackage models were manually tested
    
    - 'dev' profile was activated in the 'application.properties' file

--- src/main/java/com/example/explorerio/DevTestRunner.java
+++ src/main/java/com/example/explorerio/DevTestRunner.java
@@ -2,17 +2,20 @@ package com.example.explorerio;
 
 import com.example.explorerio.domain.model.Difficulty;
 import com.example.explorerio.domain.model.Region;
+import com.example.explorerio.domain.model.TourPackage;
 import com.example.explorerio.domain.model.Trail;
-import com.example.explorerio.domain.model.TrailPackage;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("dev")
 @Component
-public class CLRunner implements CommandLineRunner {
+public class DevTestRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //Testing Trail and TrailPackages Models
-        {TrailPackage trailPem = new TrailPackage("PEM", "Parque Estadual do Mendanha");
+        {
+            TourPackage trailPem = new TourPackage("PEM", "Parque Estadual do Mendanha",80.0);
 
             Trail t1 = new Trail(
                     "Cachoeira do Mendanha",
@@ -20,8 +23,8 @@ public class CLRunner implements CommandLineRunner {
                     "piscina, floresta, cachoeira, parque, mendanha",
                     5,
                     4.5,
-                    Difficulty.valueOf("medium".toUpperCase()),
-                    Region.valueOf("north".toUpperCase()),
+                    Difficulty.findByLevel(2),
+                    Region.findByLabel("west"),
                     trailPem
             );
             System.out.println(t1.toString());}
