5057b6c13617336bbefef94b2185ca27f1d6f38f
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-22 18:28:16 -0300

    feat: add TrailRequestDTO

---
+++ src/main/java/com/example/explorerio/application/dto/TrailRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.explorerio.application.dto;
+
+public class TrailRequestDTO {
+    public String name;
+    public String description;
+    public String keywords;
+    public int distance;
+    public double time;
+    public String difficulty;
+    public String region;
+    public String tourPackageCode;
+}
