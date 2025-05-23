260b2844bdf221a14563799b24de8337b71e304e
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-23 00:06:27 -0300

    test: modify DatabaseSeeder class.
    - Testing Trail and Tour Package seeding from JSON files

--- src/main/java/com/example/explorerio/DevTestRunner.java
+++ src/main/java/com/example/explorerio/DevTestRunner.java
@@ -1,9 +1,8 @@
 package com.example.explorerio;
 
+import com.example.explorerio.devtools.seeder.DatabaseSeeder;
 import com.example.explorerio.application.service.TourPackageService;
 import com.example.explorerio.application.service.TrailService;
-import com.example.explorerio.domain.model.Difficulty;
-import com.example.explorerio.domain.model.Region;
 import com.example.explorerio.domain.model.TourPackage;
 import com.example.explorerio.domain.model.Trail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,7 @@ public class DevTestRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //Testing Trail and TrailPackages Models
+        // Testing Trail and TrailPackages Models
         {/*TourPackage trailPem = new TourPackage("PEM", "Parque Estadual do Mendanha",80.0);
 
             Trail t1 = new Trail(
@@ -38,18 +37,29 @@ public class DevTestRunner implements CommandLineRunner {
             );
             System.out.println(t1.toString());*/}
 
-        //Testing Tour Package Service
-        {tourPackageService.createTourPackage("RJPEM1", "Circuito Cachoeiras Parque Estadual do Mendanha", 80.0);
+        // Testing Tour Package Service Manually
+        {/*tourPackageService.createTourPackage("RJPEM1", "Circuito Cachoeiras Parque Estadual do Mendanha", 80.0);
             tourPackageService.createTourPackage("RJPEM2", "Circuito Completo Cachoeiras Parque Estadual do Mendanha", 120.0);
+            tourPackageService.createTourPackage("RJTRZ1", "Trilha Pedra da Gávea", 150.0);
+            tourPackageService.createTourPackage("RJTRZ2", "Trilha Pedra Bonita com Voo de Asa Delta (opcional)", 130.0);
+            tourPackageService.createTourPackage("RJTRZ3", "Trilha Morro Dois Irmãos com Guia", 100.0);
+            tourPackageService.createTourPackage("RJTRZ4", "Trilha Floresta da Tijuca – Pico da Tijuca", 120.0);
+            tourPackageService.createTourPackage("RJTRZ5", "Trilha da Urca até o Pão de Açúcar", 90.0);
+            tourPackageService.createTourPackage("RJTRZ6", "Trilha da Praia do Perigoso e Pedra da Tartaruga", 110.0);
+            tourPackageService.createTourPackage("RJTRZ7", "Trilha da Pedra do Telégrafo com nascer do sol", 140.0);
+            tourPackageService.createTourPackage("RJTRZ8", "Trilha Circuito das Praias Selvagens (Guaratiba)", 95.0);
+            tourPackageService.createTourPackage("RJTRZ9", "Trilha da Cachoeira do Horto + Vista Chinesa", 85.0);
+            tourPackageService.createTourPackage("RJTRZ10", "Trilha Pico do Papagaio – Ilha Grande", 160.0);
+
             var myTtours = tourPackageService.getAllTourPackages();
             myTtours.forEach(System.out::println);
 
             var tour1 = tourPackageService.getTourPackageByCode("RJPEM11");
             System.out.println("Resultado da busca: \n"+
-                    (tour1.isPresent() ? tour1.get().toString() : "Tour não encontrado."));}
+                    (tour1.isPresent() ? tour1.get().toString() : "Tour não encontrado."));*/}
 
-        //Testing Trail Service
-        {trailService.createTrail(
+        // Testing Trail Service manually
+        {/*trailService.createTrail(
                     "Cachoeira do Mendanha",
                     "Cachoeira localizada no Parque Estadual do Mendanha, com várias quedas de água com piscinas naturais e uma trilha com uma bela vista da mata atlântica.",
                     "piscina, floresta, cachoeira, parque, mendanha",4,2.0,"medium","west","RJPEM2");
@@ -71,11 +81,27 @@ public class DevTestRunner implements CommandLineRunner {
 
             trailService.deleteTrail(1L);
             myTrails = trailService.getAllTrails();
+            myTrails = trailService.getAllTrails();
+            myTrails.forEach(System.out::println);
+            System.out.println("*".repeat(80) + "\n");*/}
+
+        // Testing Trail and Tour Package seeding - JSON file
+        {
+            DatabaseSeeder databaseSeeder = new DatabaseSeeder(trailService,tourPackageService);
+            databaseSeeder.readTourPackagesJsonFile("src/main/resources/tourPackages.json");
+            databaseSeeder.readTrailsJsonFile("src/main/resources/trails.json");
+
+            List<TourPackage> toursPacks = tourPackageService.getAllTourPackages();
+            toursPacks.forEach(System.out::println);
+            System.out.println("*".repeat(80) + "\n");
+
+            List<Trail> myTrails = trailService.getAllTrails();
             myTrails.forEach(System.out::println);
             System.out.println("*".repeat(80) + "\n");
+
         }
 
 
-
     }
+
 }
