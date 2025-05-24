dd7af432f0e7c2e4132123060a6be3326805ea21
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-24 17:02:23 -0300

    refactor: add annotations to the Trail Repository

--- src/main/java/com/example/explorerio/repository/TrailRepository.java
+++ src/main/java/com/example/explorerio/repository/TrailRepository.java
@@ -4,9 +4,11 @@ import com.example.explorerio.domain.model.Trail;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(exported = true, path = "trails", collectionResourceRel = "trails")
 public interface TrailRepository extends JpaRepository<Trail, Long> {
     List<Trail> findByTourPackageCode(
             @Param("tour-code") String tourPackageCode,
