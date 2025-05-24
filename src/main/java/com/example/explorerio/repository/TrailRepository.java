0e3b82d4c103665460b4d65307a04e80103a5566
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-24 16:39:54 -0300

    feat: add query method findByTourPackageCode

--- src/main/java/com/example/explorerio/repository/TrailRepository.java
+++ src/main/java/com/example/explorerio/repository/TrailRepository.java
@@ -1,7 +1,6 @@
 package com.example.explorerio.repository;
 
 import com.example.explorerio.domain.model.Trail;
-import org.hibernate.query.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +8,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TrailRepository extends JpaRepository<Trail, Long> {
-    List<Trail> findByTourPackageCode(@Param("tour-code") String tourPackageCode);
+    List<Trail> findByTourPackageCode(
+            @Param("tour-code") String tourPackageCode,
+            Pageable pageable
+            );
+
 }
