59574079976a96564f02f319ea20232c84ab332a
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-24 00:46:41 -0300

    feat: add new query methods.
    - Create findByNameContainsIgnoreCase()
    - Create findByNameContainsIgnoreCase()

--- src/main/java/com/example/explorerio/repository/TourPackageRepository.java
+++ src/main/java/com/example/explorerio/repository/TourPackageRepository.java
@@ -2,6 +2,11 @@ package com.example.explorerio.repository;
 
 import com.example.explorerio.domain.model.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
+    List<TourPackage> findByNameContainsIgnoreCase(@Param("name")String name);
+    List<TourPackage> findByPriceLessThanEqual(@Param("price-less-than") Double price);
 }
