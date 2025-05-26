68f44c27b571ba94c2b7ced8b8e1959efc949ab3
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-05-26 14:55:53 -0300

    refactor: Refactor class
    - Add Lombok annotations to the TourPackageResponseDTO
    - Extend RepresentationModel to implement HATEOAS

--- src/main/java/com/example/explorerio/application/dto/tourpackage/TourPackageResponseDTO.java
+++ src/main/java/com/example/explorerio/application/dto/tourpackage/TourPackageResponseDTO.java
@@ -3,11 +3,12 @@ package com.example.explorerio.application.dto.tourpackage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TourPackageResponseDTO {
+public class TourPackageResponseDTO extends RepresentationModel<TourPackageResponseDTO> {
     String code;
     String name;
     Double price;
